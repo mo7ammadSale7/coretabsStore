86940ce70e5e5762a246e76f323e524bc74ed95d
Mohammed Saleh <mo7mmad57@gmail.com>
2020-09-22 14:12:11 +0200

    write info file about commands, plugins and shortcuts

--- products/views.py
+++ products/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.utils import timezone
 
 
-
 # Create your views here.
 def say_hi(request, name):
     return render(request, 'say-hi.html', {'name': name})
+
 
 def show_time(request):
     now = timezone.now()
