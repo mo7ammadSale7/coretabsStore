8d4148d4f144b9312c2798b7e76096ab0f3b21e3
Mohammed Saleh <mo7mmad57@gmail.com>
2020-09-23 19:49:12 +0200

    create product details file & urls for products app

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Product
@@ -10,4 +10,12 @@ def products_list(request):
     context = {
         "products": products,
     }
-    return render(request, 'products-list.html', context)
+    return render(request, 'products/products-list.html', context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        "product": product,
+    }
+    return render(request, 'products/product-details.html', context)
