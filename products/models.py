f5b118eabdb13b6e772785a94be15e02b58e1791
Mohammed Saleh <mo7mmad57@gmail.com>
2020-09-23 18:36:21 +0200

    modified models

--- products/models.py
+++ products/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.title
