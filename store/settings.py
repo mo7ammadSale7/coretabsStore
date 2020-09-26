2151e215ee2352239b7684842c5e2a5396728a74
Mohammed Saleh <mo7mmad57@gmail.com>
2020-09-26 03:44:42 +0200

    project deploy

--- store/settings.py
+++ store/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+ALLOWED_HOSTS = ['.herokuapp.com',]
