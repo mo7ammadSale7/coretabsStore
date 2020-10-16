8d33c30e2cba862e88320b3bdf4860cca94d5afa
Mohammed Saleh <mo7mmad57@gmail.com>
2020-10-16 15:30:14 +0200

    email settings

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 # from django.core.mail import send_mail
 from .forms import OrderForm
-from .utils import send_email
 from orders.utils import send_order_email
 
 
